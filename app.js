062b50eac730a20da9e3793027297b2dddb26934
unknown <anton.ostopovici@tarainteractive.com>
2022-04-05 10:06:08 +0300

    deploy

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.use("/devices", devicesRoutes);
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ku5rn.mongodb.net/${process.env.MONGO_NAME}?retryWrites=true&w=majority`
+    // `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ku5rn.mongodb.net/${process.env.MONGO_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://Anton:No4XkJVQILLqJzCc@cluster0.ku5rn.mongodb.net/Notifications-React?retryWrites=true&w=majority`
   )
 
   .then(() => {
