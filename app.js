e58acc85fa8c926c48e7bd34753e1422d88d5a54
unknown <anton.ostopovici@tarainteractive.com>
2022-04-05 12:22:55 +0300

    deploy

--- app.js
+++ app.js
@@ -5,13 +5,24 @@ const cors = require("cors");
 
 const PORT = 8000;
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(bodyParser.json());
 require("dotenv").config();
 
 //routes
-
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 // const partnersRoutes = require("./routes/partners");
 // const userRoutes = require("./routes/users");
 const devicesRoutes = require("./routes/devices");
